7548976d0fd1c413300e9b6575de4daa52f1a313
Gabriele <41295383+gabrielezi@users.noreply.github.com>
2019-10-16 12:35:43 +0300

    pizza delegate c#

---
+++ PizzaDelegate/PizzaDelegate/Components/IVegetarian.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaDelegate
+{
+    interface IVegetarian
+    {
+        bool CheckIfChangeable(List<string> ingredientList);
+        List<string> ReplaceIngredients(List<string> ingredientList);
+        void RemoveIngredients(List<string> ingredientList);
+    }
+}
---
+++ PizzaDelegate/PizzaDelegate/Components/PizzaStone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaDelegate
+{
+    class PizzaStone : IPizzaStone
+    {
+        public void Prepare()
+        {
+            Console.WriteLine("Reaching the needed temperature");
+        }
+        public void Cook()
+        {
+            Console.WriteLine("The dish is cooked on a pizza stone");
+        }
+    }
+}
