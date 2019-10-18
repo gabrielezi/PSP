42cedf7280a8fb0777c19b9099174dbff607782a
Gabriele <41295383+gabrielezi@users.noreply.github.com>
2019-10-18 10:46:07 +0300

    no specific pizza classes

--- scala/src/main/scala/main.scala
+++ scala/src/main/scala/main.scala
@@ -1,5 +1,5 @@
 abstract class Pizza {
-  //var ingredientList : List[String]
+  var ingredientList : List[String] = _
   def MakePizza(ingredients: List[String]) = {
       if (CheckIfChangeable(ingredients)) {
         var list = ReplaceIngredients(ingredients)
@@ -7,7 +7,7 @@ abstract class Pizza {
       }
     else println(ingredients)
   }
-  //def SetIngredients(ingredients: List[String]) = { ingredientList = ingredients } //setter
+  def SetIngredients(ingredients: List[String]) = { ingredientList = ingredients } 
   protected def CheckIfChangeable(ingredients: List[String]): Boolean
   protected def ReplaceIngredients(ingredients: List[String]): List[String]
   protected def RemoveIngredients(ingredients: List[String]): Unit
@@ -20,7 +20,7 @@ abstract class Pizza {
   protected def Cook(): Unit
 }
 abstract class Coffee {
-  //var ingredientList : List[String]
+  var ingredientList : List[String] = _
   def MakeCoffee(ingredients: List[String]) = {
       if (CheckIfChangeable(ingredients)) {
         var list = ReplaceIngredients(ingredients)
@@ -28,86 +28,71 @@ abstract class Coffee {
       }
     else println(ingredients)
   }
-  //def SetIngredients(ingredients: List[String]) = { ingredientList = ingredients } //setter
+  def SetIngredients(ingredients: List[String]) = { ingredientList = ingredients } 
   protected def CheckIfChangeable(ingredients: List[String]): Boolean
   protected def ReplaceIngredients(ingredients: List[String]): List[String]
   protected def RemoveIngredients(ingredients: List[String]): Unit
 }
 
 trait Vegetarian {
-  def CheckIfChangeable(ingredients: List[String]): Boolean = {
+  protected def CheckIfChangeable(ingredients: List[String]): Boolean = {
     if(ingredients.contains("pepperoni") ||ingredients.contains("salmon"))
     return true
     else false
   }
-  def ReplaceIngredients(ingredients: List[String]): List[String] = {
+  protected def ReplaceIngredients(ingredients: List[String]): List[String] = {
     val newList=ingredients.map { case "pepperoni" => "mushroom"; case x => x }
     return newList
   }
-  def RemoveIngredients(ingredients: List[String]) = {
+  protected def RemoveIngredients(ingredients: List[String]) = {
   val newList = ingredients.filter(_ != "salmon")
     println(newList)
   }
 }
 trait Vegan {
-  def CheckIfChangeable(ingredients: List[String]): Boolean = {
+  protected def CheckIfChangeable(ingredients: List[String]): Boolean = {
     if(ingredients.contains("milk") ||ingredients.contains("egg"))
     return true
     else false
   }
-  def ReplaceIngredients(ingredients: List[String]): List[String] = {
+  protected def ReplaceIngredients(ingredients: List[String]): List[String] = {
     val newList=ingredients.map { case "milk" => "almondMilk"; case "cheese" => "veganCheese"; case x => x}
     return newList
   }
-  def RemoveIngredients(ingredients: List[String]) = {
+  protected def RemoveIngredients(ingredients: List[String]) = {
   val newList = ingredients.filter(_ != "egg")
     println(newList)
   }
 }
 trait NoneDiet {
-  def CheckIfChangeable(ingredients: List[String]): Boolean = {
+  protected def CheckIfChangeable(ingredients: List[String]): Boolean = {
     return false
   }
-  def ReplaceIngredients(ingredients: List[String]): List[String] = {
+  protected def ReplaceIngredients(ingredients: List[String]): List[String] = {
     return ingredients
   }
-  def RemoveIngredients(ingredients: List[String]) = {
+  protected def RemoveIngredients(ingredients: List[String]) = {
     println(ingredients)
   }
 }
 
 trait PizzaStone {
-  def Prepare() = {println("Reaching the needed temperature")}
-  def Cook() = {println("The pizza is cooked on a pizza stone")}
+  protected def Prepare() = {println("Reaching the needed temperature")}
+  protected def Cook() = {println("The pizza is cooked on a pizza stone")}
 }
 trait WoodFiredOven {
-  def Prepare() = {println("Reaching the needed temperature")}
-  def Cook() = {println("The pizza is cooked in a wood fired oven")}
+  protected def Prepare() = {println("Reaching the needed temperature")}
+  protected def Cook() = {println("The pizza is cooked in a wood fired oven")}
 }
 
-class Vegetarian_PizzaStonePizza extends Pizza with Vegetarian with PizzaStone{}
-class Vegetarian_WoodFiredOvenPizza extends Pizza with Vegetarian with WoodFiredOven{}
-class Vegan_PizzaStonePizza extends Pizza with Vegan with PizzaStone{}
-class Vegan_WoodFiredOvenPizza extends Pizza with Vegan with WoodFiredOven{}
-class NoneDiet_PizzaStonePizza extends Pizza with NoneDiet with PizzaStone{}
-class NoneDiet_WoodFiredOvenPizza extends Pizza with NoneDiet with WoodFiredOven{}
-
-class VegetarianCoffee extends Coffee with Vegetarian
-class VeganCoffee extends Coffee with Vegan
-class NoneDietCoffee extends Coffee with NoneDiet
-
-
 val list = List("sauce", "pepperoni", "salmon", "milk")
-//var veg_pizzaStone = new Vegetarian_PizzaStonePizza();
-//veg_pizzaStone.MakePizza(list)
-//veg_pizzaStone.Bake()
 
 val coffeeList = List("coffee", "milk")
-val coffee = new VeganCoffee()
-coffee.MakeCoffee(coffeeList)
-
-//------be klasiu
+val veganCoffee = new Coffee() with Vegan
+veganCoffee.SetIngredients(coffeeList)
+veganCoffee.MakeCoffee(veganCoffee.ingredientList)
 
 val vegetarian_pizzaStone = new Pizza with Vegetarian with PizzaStone
-vegetarian_pizzaStone.MakePizza(list)
+vegetarian_pizzaStone.SetIngredients(list)
+vegetarian_pizzaStone.MakePizza(vegetarian_pizzaStone.ingredientList)
 vegetarian_pizzaStone.Bake()
